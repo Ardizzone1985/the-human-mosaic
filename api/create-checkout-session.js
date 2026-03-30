238921e1fabb4c79d54e01d9c57b604395268199
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-03-30 23:01:57 +0200

    Create create-checkout-session.js

---
+++ api/create-checkout-session.js
@@ -0,0 +1,38 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  const { price, room, slot } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+
+      line_items: [
+        {
+          price_data: {
+            currency: "eur",
+            product_data: {
+              name: `The Human Mosaic - ${room} Room`,
+            },
+            unit_amount: price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+
+      success_url: `https://thehumanmosaic.art/upload.html?room=${room}&slot=${slot}`,
+      cancel_url: `https://thehumanmosaic.art/checkout.html`,
+    });
+
+    res.status(200).json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
