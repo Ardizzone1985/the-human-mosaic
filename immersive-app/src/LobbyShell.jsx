b3e2beb0c2768c2f671c39d10b48b5a3efdabc08
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-06-22 20:49:54 +0200

    Update LobbyShell.jsx

--- immersive-app/src/LobbyShell.jsx
+++ immersive-app/src/LobbyShell.jsx
@@ -179,11 +179,11 @@ metalness={0.48}
   <mesh>
     <boxGeometry args={[4.5, 8.2, 0.08]} />
     <meshStandardMaterial
-      color="#241008"
+      color="#2f150b"
       roughness={0.52}
       metalness={0.08}
       emissive="#120603"
-      emissiveIntensity={0.10}
+      emissiveIntensity={0.18}
     />
   </mesh>
 
