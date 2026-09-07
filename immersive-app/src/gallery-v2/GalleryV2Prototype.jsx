5b356d603b4fb19d75facf2c3abe9577bad3bcfb
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-07 21:21:53 +0200

    Update GalleryV2Prototype.jsx

--- immersive-app/src/gallery-v2/GalleryV2Prototype.jsx
+++ immersive-app/src/gallery-v2/GalleryV2Prototype.jsx
@@ -1,8 +1,12 @@
+import PrototypeWall from "./PrototypeWall";
+
 export default function GalleryV2Prototype() {
   return (
     <div>
       <h1>Gallery v2 Prototype</h1>
       <p>Isolated development environment.</p>
+
+      <PrototypeWall />
     </div>
   );
 }
