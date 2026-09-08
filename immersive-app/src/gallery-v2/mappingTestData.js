690ba5eef7c891513e35cee9c7c358234292e0db
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-08 18:29:41 +0200

    Create mappingTestData.js

---
+++ immersive-app/src/gallery-v2/mappingTestData.js
@@ -0,0 +1,26 @@
+export const mappingTestSlots = [
+  {
+    slot_code: "Identity-F1-R2-C6",
+    room: "Identity",
+    wall: "Front Wall",
+    section: "F1",
+    row_number: 2,
+    col_number: 6,
+  },
+  {
+    slot_code: "Identity-F1-R1-C1",
+    room: "Identity",
+    wall: "Front Wall",
+    section: "F1",
+    row_number: 1,
+    col_number: 1,
+  },
+  {
+    slot_code: "Identity-L1-R22-C3",
+    room: "Identity",
+    wall: "Left Wall",
+    section: "L1",
+    row_number: 22,
+    col_number: 3,
+  },
+];
