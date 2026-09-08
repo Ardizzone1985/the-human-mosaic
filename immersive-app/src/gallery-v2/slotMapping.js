4d61d6560a6406486f8ab1acf3d4d033b323143d
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-08 18:22:48 +0200

    Update slotMapping.js

--- immersive-app/src/gallery-v2/slotMapping.js
+++ immersive-app/src/gallery-v2/slotMapping.js
@@ -6,3 +6,20 @@ export function createGalleryPosition(sectionNumber, row, column) {
     galleryKey: `S${sectionNumber}-R${row}-C${column}`,
   };
 }
+
+export function galleryPositionFromIndex(index) {
+  const SLOTS_PER_SECTION = 600;
+  const COLUMNS = 10;
+
+  const sectionNumber = Math.floor(index / SLOTS_PER_SECTION) + 1;
+  const indexInSection = index % SLOTS_PER_SECTION;
+
+  const row = Math.floor(indexInSection / COLUMNS) + 1;
+  const column = (indexInSection % COLUMNS) + 1;
+
+  return createGalleryPosition(
+    sectionNumber,
+    row,
+    column
+  );
+}
