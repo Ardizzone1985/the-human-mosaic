32b925a80f9700074a50b8c471a3f19ddb0120b0
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-12 19:54:38 +0200

    Update GalleryWall3D.jsx

--- immersive-app/src/gallery-v2/GalleryWall3D.jsx
+++ immersive-app/src/gallery-v2/GalleryWall3D.jsx
@@ -1,3 +1,13 @@
 export default function GalleryWall3D() {
-  return null;
+  return (
+    <mesh position={[0, 2, -4]}>
+      <planeGeometry args={[6, 4]} />
+
+      <meshStandardMaterial
+        color="#e8e3d8"
+        roughness={0.75}
+        metalness={0.05}
+      />
+    </mesh>
+  );
 }
