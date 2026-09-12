eb1051eab73b77e39b782420f3093b15616e31ca
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-12 20:01:21 +0200

    Update main.jsx

--- immersive-app/src/main.jsx
+++ immersive-app/src/main.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import GalleryV2Prototype from "./gallery-v2/GalleryV2Prototype.jsx";
+import GalleryWall3DPrototype from "./gallery-v2/GalleryWall3DPrototype.jsx";
 import "./style.css";
 import { AuthProvider } from "./auth/AuthProvider.jsx";
 
 const pathname = window.location.pathname.replace(/\/+$/, "");
 const isGalleryV2 = pathname === "/gallery-v2";
+const isGalleryV23D = pathname === "/gallery-v2-3d";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isGalleryV2 ? (
-      <GalleryV2Prototype />
-    ) : (
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    )}
+    {isGalleryV23D ? (
+  <GalleryWall3DPrototype />
+) : isGalleryV2 ? (
+  <GalleryV2Prototype />
+) : (
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+)}
   </React.StrictMode>
 );
