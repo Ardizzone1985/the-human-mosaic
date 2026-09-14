15e929fe32e75ac9669151ad3db9a74ceb33efa5
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-14 12:35:24 +0200

    Update GalleryWall3D.jsx

--- immersive-app/src/gallery-v2/GalleryWall3D.jsx
+++ immersive-app/src/gallery-v2/GalleryWall3D.jsx
@@ -1,6 +1,6 @@
 export default function GalleryWall3D() {
   return (
-    <mesh position={[0, 2, -4]}>
+    <mesh position={[0, 0, -4]}>
       <planeGeometry args={[6, 4]} />
 
       <meshStandardMaterial
