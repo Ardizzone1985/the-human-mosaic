d5af89664006d54f30910bfbb05e487ee8087957
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-15 21:14:17 +0200

    Update GalleryWall3DPrototype.jsx

--- immersive-app/src/gallery-v2/GalleryWall3DPrototype.jsx
+++ immersive-app/src/gallery-v2/GalleryWall3DPrototype.jsx
@@ -46,6 +46,20 @@ export default function GalleryWall3DPrototype() {
   Previous Section
 </Text>
 
+        <Text
+  position={[1.5, -2, -3.85]}
+  fontSize={0.22}
+  color="#222222"
+  anchorX="center"
+  anchorY="middle"
+  onClick={() => {
+    setSectionNumber((current) => Math.min(1667, current + 1));
+    setViewNumber(1);
+  }}
+>
+  Next Section
+</Text>
+
         {/* pavimento */}
         <mesh
   position={[0, -2, 1]}
