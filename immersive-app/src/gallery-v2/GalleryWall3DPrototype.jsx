d14b9dd117fcaf282f09244974efa0c7b2ba3a7f
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-15 19:15:17 +0200

    Update GalleryWall3DPrototype.jsx

--- immersive-app/src/gallery-v2/GalleryWall3DPrototype.jsx
+++ immersive-app/src/gallery-v2/GalleryWall3DPrototype.jsx
@@ -17,7 +17,7 @@ export default function GalleryWall3DPrototype() {
           intensity={1.5}
         />
 
-        <GalleryWall3D />
+        <GalleryWall3D sectionNumber={2} />
 
         {/* pavimento */}
         <mesh
