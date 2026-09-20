afa6c2247a1d1933e57452b49adce0f62cce0417
Ardizzone1985 <g.ardizzone85@hotmail.it>
2026-09-20 13:21:17 +0200

    Update GalleryPhotoWall3D.jsx

--- immersive-app/src/gallery-v2/GalleryPhotoWall3D.jsx
+++ immersive-app/src/gallery-v2/GalleryPhotoWall3D.jsx
@@ -24,9 +24,7 @@ rotation={
 }
 >
       <mesh>
-  <planeGeometry
-    args={wall === "Front" ? [22, 8] : [14, 8]}
-  />
+  <planeGeometry args={[22, 8]} />
         <meshStandardMaterial
           color="#e8e3d8"
           roughness={0.75}
